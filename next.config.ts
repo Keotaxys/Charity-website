d92f9f38cf539bda292cb3f6c3480c2b2d207fd1
keota.mar <keota.mar@gmail.com>
2026-03-11 14:12:54 +0000

    commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 import type { NextConfig } from "next";
 
 // ສ້າງ Plugin ຂອງ next-intl (ມັນຈະໄປຊອກຫາໄຟລ໌ src/i18n.ts ອັດຕະໂນມັດ)
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 const nextConfig: NextConfig = {
   // ຖ້າມີການຕັ້ງຄ່າອື່ນໆໃນອະນາຄົດ ເຊັ່ນ: ການອະນຸຍາດລິ້ງຮູບພາບຈາກ Firebase ສາມາດມາໃສ່ບ່ອນນີ້ໄດ້
